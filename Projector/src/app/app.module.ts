bce027b75d571967d65d164526492550382a689b
Orfrid <orfridman10@gmail.com>
2020-12-31 02:01:25 +0200

    added navbar

--- Projector/src/app/app.module.ts
+++ Projector/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -26,6 +28,8 @@ import { MatMenuModule } from '@angular/material/menu';
     MatSlideToggleModule,
     MatMenuModule,
     FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Projector/src/app/navigation/navigation.component.ts
+++ Projector/src/app/navigation/navigation.component.ts
@@ -11,5 +11,7 @@ export class NavigationComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  stuff() {
+    console.log('clicked')
+  }
 }
