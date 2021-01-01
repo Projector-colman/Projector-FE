5357a326a05337f4965db1d3c768f5cba3a4bb98
itamar-marom <marom.itamar@gmail.com>
2021-01-01 18:13:19 +0200

    better location for project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Projector'),
+      dir: require('path').join(__dirname, './coverage/Projector-FE'),
       subdir: '.',
       reporters: [
         { type: 'html' },
