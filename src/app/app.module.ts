c71d4532a1bca6cd48c0d43c3cb3e352d01f33be
Orfrid <orfridman10@gmail.com>
2021-01-04 10:34:50 +0200

    added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { MainPageComponent } from './views/main-page/main-page.component';
+import { MyProjectsComponent } from './views/my-projects/my-projects.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'projects', component: MyProjectsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainPageComponent,
+    MyProjectsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes,
+      {enableTracing: true} // for debugging
+      ),
   ],
   providers: [],
   bootstrap: [AppComponent]
