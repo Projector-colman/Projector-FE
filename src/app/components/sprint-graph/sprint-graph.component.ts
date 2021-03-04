a1222097f69310590f87bff338e3c17c7abca47a
shahar freidenberg <shahar.freidenberg@gmail.com>
2021-03-04 20:37:17 +0200

    fix

---
+++ src/app/components/sprint-graph/sprint-graph.component.ts
@@ -0,0 +1,88 @@
+import { Component, ViewChild } from "@angular/core";
+
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexDataLabels,
+  ApexTitleSubtitle,
+  ApexStroke,
+  ApexGrid
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+};
+
+@Component({
+  selector: "app-sprint-graph",
+  templateUrl: "./sprint-graph.component.html",
+  styleUrls: ["./sprint-graph.component.scss"]
+})
+export class SprintGraphComponent {
+  @ViewChild("chart") chart: ChartComponent;
+  public chartOptions: Partial<ChartOptions>;
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          name: "Planned",
+          data: [78, 72, 66, 60, 54, 48, 42, 36, 30, 24, 18, 12, 6, 0]
+        },
+        {
+          name: "Real",
+          data: [78, 74, 72, 64, 60, 54, 50, 48, 40, 38, 36, 30, 24, 20]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+      title: {
+        text: "My Sprint Worksheet",
+        align: "left"
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5
+        }
+      },
+      xaxis: {
+        categories: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14"
+        ]
+      }
+    };
+  }
+}
