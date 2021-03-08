4d5b6c582cf02246d493c5885448df623338b0ea
Orfrid <orfridman10@gmail.com>
2021-03-08 19:31:09 +0200

    god damn it

--- src/app/views/backlog/backlog.component.ts
+++ src/app/views/backlog/backlog.component.ts
@@ -1,8 +1,10 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IssueLocation } from 'src/app/enum/issueLocation.enum';
 import { Issue } from 'src/app/interfaces/issue';
 import { Project } from 'src/app/interfaces/project';
+import { IssueStatus } from 'src/app/enum/issueStatus.enum';
 import { ProjectsService } from 'src/app/services/projects.service';
 import { SidenavUpdateService } from 'src/app/services/sidenav-update.service';
 
@@ -36,5 +38,52 @@ export class BacklogComponent implements OnInit {
 
   public get issuesLocation(): typeof IssueLocation {
     return IssueLocation;
+  }
+  drop(event: CdkDragDrop<Issue[]>) {
+    if (event.container.id !== event.previousContainer.id) {
+      let list: Issue[];
+      switch (event.previousContainer.id) {
+        case 'CurrSprint': {
+          list = this.getStatusIssues(this.issuesLocation.CurrentSprint);
+          break;
+        }
+        case 'PlannedStrint': {
+          list = this.getStatusIssues(this.issuesLocation.PlannedSprint);
+          break;
+        }
+        case 'Backlog': {
+          list = this.getStatusIssues(this.issuesLocation.Backlog);
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+
+      switch (event.container.id) {
+        case 'CurrSprint': {
+          list[
+            event.previousIndex
+          ].location = this.issuesLocation.CurrentSprint;
+          list[event.previousIndex].status = IssueStatus.ToDo;
+          break;
+        }
+        case 'PlannedStrint': {
+          list[
+            event.previousIndex
+          ].location = this.issuesLocation.PlannedSprint;
+          list[event.previousIndex].status = IssueStatus.None;
+          break;
+        }
+        case 'Backlog': {
+          list[event.previousIndex].location = this.issuesLocation.Backlog;
+          list[event.previousIndex].status = IssueStatus.None;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    }
   }
 }
