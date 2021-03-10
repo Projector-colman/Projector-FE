df482f3ce488b576ee7170cca79dce9df1e37562
YotamNordman <yotam.nordman@gmail.com>
2021-03-10 18:55:42 +0200

    create project modal

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { User } from 'src/app/interfaces/user';
 import { UsersService } from 'src/app/services/users.service';
+import { CreateProjectComponent } from '../modals/create-project/create-project.component';
 import { CreateIssueComponent } from '../modals/create-issue/create-issue.component';
 
 @Component({
@@ -17,7 +18,11 @@ export class NavigationComponent implements OnInit {
   getCurrUser(): User {
     return this.userService.getCurrConnectedUser();
   }
-
+  createProject() {
+    const issueDialogRef = this.dialog.open(CreateProjectComponent, {
+      width: '65vh',
+      height: '75vh',
+    });}
   createIssue() {
     const issueDialogRef = this.dialog.open(CreateIssueComponent, {
       width: '65vh',
